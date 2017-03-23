a27d2a3e303677936686d90755d762ff81adfb54
Marcin Rudolf <marcin.rudolf@neufund.org>
2017-03-24 00:59:22 +0100

    unit tests: add forward and withdraw methods to employee tester, skip forwarding for empty messages

--- sol_tests/Test.Types.sol
+++ sol_tests/Test.Types.sol
@@ -1,14 +1,20 @@
 pragma solidity ^0.4.0;
 
 import "./ESOP.sol";
+import "./ERC20OptionsConverter.sol";
 
 contract EmpTester {
   address _t;
   function _target( address target ) {
     _t = target;
   }
-  function() {
-    if(!_t.call(msg.data)) throw;
+  function() payable {
+    if (msg.data.length > 0) // do not call default function on target
+      if(!_t.call(msg.data)) throw;
+  }
+
+  function forward(bytes4 signature, address a, uint256 v) returns (bool) {
+    return _t.call(signature, a, v);
   }
 
   function employeeConvertsOptions() returns (uint8){
@@ -21,6 +27,10 @@ contract EmpTester {
 
   function calcEffectiveOptionsForEmployee(address e, uint32 calcAtTime) returns (uint) {
     return ESOP(_t).calcEffectiveOptionsForEmployee(e, calcAtTime);
+  }
+
+  function withdraw() returns (uint) {
+    return ProceedsOptionsConverter(_t).withdraw();
   }
 }
 
