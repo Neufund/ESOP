3da7fbf97b02b5b6aedea75d40af0f136d73b275
Marcin Rudolf <marcin.rudolf@neufund.org>
2017-03-26 22:34:20 +0200

    unit tests: test ESOPMaker in frontier

--- sol_tests/Test.Frontier.sol
+++ sol_tests/Test.Frontier.sol
@@ -5,7 +5,7 @@ import 'dapple/reporter.sol';
 import "./Test.DummyOptionConverter.sol";
 import "./Test.Types.sol";
 
-contract TestFrontier is Test, Reporter, ESOPTypes
+contract TestFrontier is Test, ESOPMaker, Reporter, ESOPTypes
 {
     EmpTester emp1;
     ESOP esop;
@@ -13,7 +13,17 @@ contract TestFrontier is Test, Reporter, ESOPTypes
   function setUp() {
     emp1 = new EmpTester();
     //emp2 = new Tester();
-    esop = new ESOP();
-    emp1._target(esop);
+    //esop = new ESOP();
+    //emp1._target(esop);
+  }
+
+  function testESOPInstantiation() {
+    RootOfTrust root = new RootOfTrust();
+    ESOP e = new ESOP(address(this), address(root));
+    root.setESOP(e);
+    bytes memory poolEstablishmentDocIPFSHash = "qmv8ndh7ageh9b24zngaextmuhj7aiuw3scc8hkczvjkww";
+    // make CEO sign this
+    uint rc = uint(e.openESOP(1 years, 4 years, 8000, 2000, 1000, 1000000, poolEstablishmentDocIPFSHash));
+    assertEq(rc, 0);
   }
 }
