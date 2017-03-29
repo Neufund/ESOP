b2ebdfa6b974456f56d8e8ac6df6864cd6fe889f
Marcin Rudolf <marcin.rudolf@neufund.org>
2017-03-29 20:30:06 +0200

    disabled deployment via nano on parity (from:)

--- truffle.js
+++ truffle.js
@@ -11,7 +11,7 @@ module.exports = {
       host: "localhost",
       port: 8444,
       network_id: "17",
-      from: "0xFAFfd72A5fc6375eac399cce6141210723bd8889" // my nano S
+      // from: "0xFAFfd72A5fc6375eac399cce6141210723bd8889" // my nano S
     },
     "live": {
       network_id: 1 // Ethereum public network
