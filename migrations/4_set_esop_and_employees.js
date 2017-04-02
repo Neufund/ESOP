8ab433cb675b4c052cdf40585db73385a3c490ea
Marcin Rudolf <marcin.rudolf@neufund.org>
2017-04-03 00:17:34 +0200

    get return error codes from events in deployments scripts when calling non-const functions

--- migrations/4_set_esop_and_employees.js
+++ migrations/4_set_esop_and_employees.js
@@ -14,16 +14,25 @@ module.exports = function (deployer, network, accounts) {
             // function openESOP(uint32 pCliffDuration, uint32 pVestingDuration, uint32 pMaxFadeoutPromille, uint32 pExitBonusPromille,
             //    uint32 pNewEmployeePoolPromille, uint32 pTotalOptions, bytes pPoolEstablishmentDocIPFSHash)
             var ipfsHash = new Buffer("QmRsjnNkEpnDdmYB7wMR7FSy1eGZ12pDuhST3iNLJTzAXF", 'ascii');
-            let rc = await esop.openESOP(1 * years, 4 * years, 8000, 2000, 1000, 1000000, web3.toBigNumber('0x' + ipfsHash.toString('hex')));
-            deployer.logger.log('esop opened with code ' + rc);
+            deployer.logger.log('opening esop');
+            let tx = await esop.openESOP(1 * years, 4 * years, 8000, 2000, 1000, 1000000, web3.toBigNumber('0x' + ipfsHash.toString('hex')));
+            if (tx.logs.some(e => e.event == 'ReturnCode')) {
+                // error code returned
+                deployer.logger.log(`openESOP returned rc: ${tx.logs[0].args['rc']}`);
+                throw `openESOP returned rc: ${tx.logs[0].args['rc']}`;
+            }
+            deployer.logger.log('esop opened');
             let ceo = await esop.addressOfCEO();
             let startdate = currdate;
             accounts.filter(a => a !== ceo).map(async function(e) {
                 // function addNewEmployeeToESOP(address e, uint32 vestingStarts, uint32 timeToSign, uint32 extraOptions, bool poolCleanup)
-                let rc = await esop.addNewEmployeeToESOP(e, startdate - 1 * weeks, startdate + 4 * weeks, 0, false);
-                console.log(rc);
-                deployer.logger.log(`employee ${e} added with rc ${rc}`);
-
+                let tx = await esop.addNewEmployeeToESOP(e, startdate - 1 * weeks, startdate + 4 * weeks, 0, false);
+                if (tx.logs.some(e => e.event === 'NewEmployee')) {
+                    deployer.logger.log(`employee ${e} added with ${tx.logs[0].args['options']} options`);
+                } else {
+                    deployer.logger.log(`addNewEmployeeToESOP returned rc: ${tx.logs[0].args['rc']}`);
+                    throw `addNewEmployeeToESOP returned rc: ${tx.logs[0].args['rc']}`;
+                }
             });
         } );
     }
