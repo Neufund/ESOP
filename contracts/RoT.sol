154b0e3137d85b0ebd611eec1cb9e6358c2cee35
Marcin Rudolf <marcin.rudolf@neufund.org>
2017-04-05 00:52:43 +0200

    added function to suicide RoT on unsupported fork

--- contracts/RoT.sol
+++ contracts/RoT.sol
@@ -3,9 +3,19 @@ import "./Types.sol";
 
 contract RoT is Ownable {
     address public ESOPAddress;
+    address public addressOfCEO;
 
     // change esop contract
-    function setESOP(address ESOP) public onlyOwner {
+    function setESOP(address ESOP, address ceo) public onlyOwner {
+      addressOfCEO = ceo;
       ESOPAddress = ESOP;
+    }
+
+    function killOnUnsupportedFork() public {
+      // this method may only be called by CEO on unsupported forks
+      if (msg.sender != addressOfCEO) throw;
+      delete ESOPAddress;
+      delete addressOfCEO;
+      selfdestruct(owner);
     }
 }
