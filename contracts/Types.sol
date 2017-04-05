db155aeddb32320bae529ad97a5da68f92aa35cc
Marcin Rudolf <marcin.rudolf@neufund.org>
2017-04-05 14:52:20 +0200

    remco's simplified formulas used in Math, TimeSource refactored

--- contracts/Types.sol
+++ contracts/Types.sol
@@ -15,7 +15,6 @@ contract Ownable {
   }
 
   function transferOwnership(address newOwner) onlyOwner {
-    // @remco: Why not transfer + accept to prevent invalid transfers?
     if (newOwner != address(0)) owner = newOwner;
   }
 }
@@ -23,53 +22,44 @@ contract Ownable {
 
 contract Math {
   // todo: should be a library
-
-  // These functions can be labeled `internal`.
-  // No need to have them in the ABI.
-
-  function divRound(uint v, uint d) public constant returns(uint) {
+  function divRound(uint v, uint d) internal constant returns(uint) {
     // round up if % is half or more
-    // Why not `(v + (d/2)) / d` ?
-    return v/d + (v % d >= (d%2 == 1 ? d/2+1 : d/2) ? 1: 0);
+    return (v + (d/2)) / d;
   }
 
   function absDiff(uint v1, uint v2) public constant returns(uint) {
-    // !! This depends on both uint-underflow and out-of-range casting to int.
-    // Both of these are not part of the Solidity language standard
-    // and can break without warning in future version.
-    // Why not `a > b ? a - b : b - a`?
-    return v1 >= v2 ? v1 - v2 : uint(-(int(v1 - v2)));
+    return v1 > v2 ? v1 - v2 : v2 - v1;
   }
 
   function safeMul(uint a, uint b) public constant returns (uint) {
-    // This overflows first, and then checks. But checking for overflow in
-    // multiply is hard.
     uint c = a * b;
     if (a == 0 || c / a == b)
       return c;
     else
       throw;
   }
+
+  function safeAdd(uint a, uint b) internal returns (uint) {
+    uint c = a + b;
+    if (!(c>=a && c>=b)) throw;
+    return c;
+  }
 }
 
 
-// TestRPC has functions for mocking the time, that would make this
-// contract uneccessary?
-contract TimeSource is Ownable {
-  uint32 mockNow; // private
+contract TimeSource {
+  uint32 private mockNow;
 
   function currentTime() public constant returns (uint32) {
     // we do not support dates much into future (Sun, 07 Feb 2106 06:28:15 GMT)
     if (block.timestamp > 0xFFFFFFFF)
       throw;
-    // alow to return debug time on test nets etc.
-    if (block.number > 3316029)
-      return uint32(block.timestamp);
-    else
-      return mockNow > 0 ? mockNow : uint32(block.timestamp);
+    return mockNow > 0 ? mockNow : uint32(block.timestamp);
   }
 
-  function mockTime(uint32 t) public onlyOwner {
+  function mockTime(uint32 t) public {
+    // no mocking on mainnet
+    if (block.number > 3316029) throw;
     mockNow = t;
   }
 }
@@ -100,7 +90,6 @@ contract Upgradeable is Ownable {
       selfdestruct(owner);
     }
 
-    // Maybe store/inform users where to migrate to?
     function beginMigration() public onlyOwner notInMigration {
         migrationState = MigrationState.OngoingMigration;
     }
