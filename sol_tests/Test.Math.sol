cbea7311b1bc0095888c5b1c80b3f0ec9a5eacf1
Marcin Rudolf <marcin.rudolf@neufund.org>
2017-04-05 14:53:09 +0200

    unit tests: test absDiff and divRound Math operations

---
+++ sol_tests/Test.Math.sol
@@ -0,0 +1,27 @@
+pragma solidity ^0.4.0;
+
+import 'dapple/test.sol';
+import 'dapple/reporter.sol';
+import "./Test.Types.sol";
+
+contract TestFrontier is Test, Reporter, Math
+{
+
+  function setUp() {
+  }
+
+  function testAbsDiff() {
+    assertEq(absDiff(5, 3), absDiff(3,5));
+    assertEq(absDiff(5, 3), 2);
+    // two's complement will be used
+    assertEq(absDiff(uint(-5), uint(-3)), 2);
+  }
+
+  function testDivRound() {
+    assertEq(divRound(1871, 11), 170, "round up");
+    assertEq(divRound(12871, 2), 6436, "round up from 0.5");
+    assertEq(divRound(10,2), 5, "no round");
+    assertEq(divRound(81542, 23), 3545, "round down");
+  }
+
+}
