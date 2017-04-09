e2815f777e3389e92f537fc109246e67179bc9ea
Marcin Rudolf <marcin.rudolf@neufund.org>
2017-04-09 21:23:36 +0200

    unit tests: test persistence of suspendedAt and terminate func signature change in EmployeesList

--- sol_tests/Test.EmployeesList.sol
+++ sol_tests/Test.EmployeesList.sol
@@ -14,9 +14,9 @@ contract TestEmployeesList is Test, Reporter, ESOPTypes
     Tester emp1 = new Tester();
     EmployeesList l = new EmployeesList();
     uint32 ct = uint32(block.timestamp);
-    bool isNew = l.setEmployee(address(emp1), ct, ct + 2 weeks, ct + 3 weeks, ct + 4 weeks, 100, 200, EmployeeState.Employed);
+    bool isNew = l.setEmployee(address(emp1), ct, ct + 2 weeks, ct + 3 weeks, ct + 4 weeks, 100, 200, 0, EmployeeState.Employed);
     assertEq(isNew, true);
-    isNew = l.setEmployee(address(emp1), ct, ct + 2 weeks, ct + 3 weeks, ct + 4 weeks, 100, 200, EmployeeState.Employed);
+    isNew = l.setEmployee(address(emp1), ct, ct + 2 weeks, ct + 3 weeks, ct + 4 weeks, 100, 200, 0, EmployeeState.Employed);
     assertEq(isNew, false);
     assertEq(uint(l.size()), 1);
     assertEq(l.addresses(0), address(emp1));
@@ -33,10 +33,10 @@ contract TestEmployeesList is Test, Reporter, ESOPTypes
     Tester emp1 = new Tester();
     EmployeesList l = new EmployeesList();
     uint32 ct = uint32(block.timestamp);
-    bool isNew = l.setEmployee(address(emp1), ct, ct + 2 weeks, ct + 3 weeks, ct + 4 weeks, 100, 200, EmployeeState.Employed);
+    bool isNew = l.setEmployee(address(emp1), ct, ct + 2 weeks, ct + 3 weeks, ct + 4 weeks, 100, 200, 0, EmployeeState.Employed);
     assertEq(isNew, true);
     Tester emp2 = new Tester();
-    isNew = l.setEmployee(address(emp2), ct, ct + 2 weeks, ct + 3 weeks, ct + 4 weeks, 1000, 2000, EmployeeState.Terminated);
+    isNew = l.setEmployee(address(emp2), ct, ct + 2 weeks, ct + 3 weeks, ct + 4 weeks, 1000, 2000, 0, EmployeeState.Terminated);
     assertEq(isNew, true);
     assertEq(uint(l.size()), 2);
     assertEq(l.addresses(0), address(emp1));
@@ -66,7 +66,7 @@ contract TestEmployeesList is Test, Reporter, ESOPTypes
     assertEq(l.addresses(1), address(0));
     assertEq(l.hasEmployee(address(emp2)), false);
     // test indexes again by adding user that was just deleted
-    isNew = l.setEmployee(address(emp2), ct, ct + 2 weeks, ct + 3 weeks, ct + 4 weeks, 100, 0, EmployeeState.Employed);
+    isNew = l.setEmployee(address(emp2), ct, ct + 2 weeks, ct + 3 weeks, ct + 4 weeks, 100, 0, 0, EmployeeState.Employed);
     assertEq(isNew, true);
     sere = l.getSerializedEmployee(address(emp2));
     assembly { emp := sere }
@@ -77,7 +77,7 @@ contract TestEmployeesList is Test, Reporter, ESOPTypes
     Tester emp1 = new Tester();
     EmployeesList l = new EmployeesList();
     uint32 ct = uint32(block.timestamp);
-    l.setEmployee(address(emp1), ct, ct + 2 weeks, ct + 3 weeks, ct + 4 weeks, 100, 200, EmployeeState.Employed);
+    l.setEmployee(address(emp1), ct, ct + 2 weeks, ct + 3 weeks, ct + 4 weeks, 100, 200, 32987, EmployeeState.Employed);
     Employee memory emp;
     var sere = l.getSerializedEmployee(address(emp1));
     assembly { emp := sere }
@@ -88,9 +88,10 @@ contract TestEmployeesList is Test, Reporter, ESOPTypes
     assertEq(uint(emp.fadeoutStarts), uint(ct + 4 weeks));
     assertEq(uint(emp.poolOptions), 100);
     assertEq(uint(emp.extraOptions), 200);
+    assertEq(uint(emp.suspendedAt), 32987);
     assertEq(uint(emp.state), uint(EmployeeState.Employed));
     // modify
-    l.terminateEmployee(address(emp1), ct + 1 years, ct + 2 years, EmployeeState.Terminated);
+    l.terminateEmployee(address(emp1), emp.issueDate, ct + 1 years, ct + 2 years, EmployeeState.Terminated);
     sere = l.getSerializedEmployee(address(emp1));
     assembly { emp := sere }
     assertEq(uint(emp.terminatedAt), uint(ct + 1 years));
@@ -102,18 +103,24 @@ contract TestEmployeesList is Test, Reporter, ESOPTypes
     assembly { emp := sere }
     assertEq(uint(emp.state), uint(EmployeeState.OptionsExercised));
     // direct mod
-    l.setEmployee(address(emp1), ct, ct + 2 weeks, ct + 3 weeks, ct + 4 weeks, 1000, 2000, EmployeeState.Employed);
+    l.setEmployee(address(emp1), ct, ct + 2 weeks, ct + 3 weeks, ct + 4 weeks, 1000, 2000, 761867, EmployeeState.Employed);
     sere = l.getSerializedEmployee(address(emp1));
     assembly { emp := sere }
     assertEq(uint(emp.poolOptions), 1000);
     assertEq(uint(emp.extraOptions), 2000);
+    assertEq(uint(emp.suspendedAt), 761867);
+    // directly write fadeout time
+    l.setFadeoutStarts(address(emp1), ct + 7 weeks);
+    sere = l.getSerializedEmployee(address(emp1));
+    assembly { emp := sere }
+    assertEq(uint(emp.fadeoutStarts), uint(ct + 7 weeks));
   }
 
   function testThrowGetNonExistingEmployee() {
     Tester emp1 = new Tester();
     EmployeesList l = new EmployeesList();
     uint32 ct = uint32(block.timestamp);
-    l.setEmployee(address(emp1), ct, ct + 2 weeks, ct + 3 weeks, ct + 4 weeks, 100, 200, EmployeeState.Employed);
+    l.setEmployee(address(emp1), ct, ct + 2 weeks, ct + 3 weeks, ct + 4 weeks, 100, 200, 0, EmployeeState.Employed);
     Tester emp2 = new Tester();
     var sere = l.getSerializedEmployee(address(emp2));
 
@@ -122,15 +129,15 @@ contract TestEmployeesList is Test, Reporter, ESOPTypes
   function testThrowTerminanteNonExitstingEmployee() {
     EmployeesList l = new EmployeesList();
     Tester emp2 = new Tester();
-    l.terminateEmployee(address(emp2), 0, 0, EmployeeState.Terminated);
+    l.terminateEmployee(address(emp2),0, 0, 0, EmployeeState.Terminated);
   }
 
   function testThrowTerminateWithInvalidState() {
     Tester emp1 = new Tester();
     EmployeesList l = new EmployeesList();
     uint32 ct = uint32(block.timestamp);
-    l.setEmployee(address(emp1), ct, ct + 2 weeks, ct + 3 weeks, ct + 4 weeks, 100, 200, EmployeeState.Employed);
-    l.terminateEmployee(address(emp1), 0, 0, EmployeeState.Employed);
+    l.setEmployee(address(emp1), ct, ct + 2 weeks, ct + 3 weeks, ct + 4 weeks, 100, 200, 0, EmployeeState.Employed);
+    l.terminateEmployee(address(emp1), ct, 0, 0, EmployeeState.Employed);
   }
 
   function testThrowChangeStateNonExistingEmployee() {
