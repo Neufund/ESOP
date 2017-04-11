68a67ea65bad8aeeb808121b5d49c8d4a9c7ea51
Marcin Rudolf <marcin.rudolf@neufund.org>
2017-04-11 14:20:28 +0200

    update deployment script to create child contracts and pass them to openESOP

--- migrations/4_set_esop_and_employees.js
+++ migrations/4_set_esop_and_employees.js
@@ -1,6 +1,8 @@
 require('babel-register');
 
 const ESOP = artifacts.require("ESOP");
+const CALCULATOR = artifacts.require("OptionsCalculator");
+const EMPLIST = artifacts.require("EmployeesList");
 const years = 365 * 24 * 60 * 60;
 const currdate = (new Date()) / 1;
 const weeks = 7 * 24 * 60 * 60;
@@ -14,8 +16,13 @@ module.exports = function (deployer, network, accounts) {
             // function openESOP(uint32 pcliffPeriod, uint32 pvestingPeriod, uint32 pMaxFadeoutPromille, uint32 pbonusOptionsPromille,
             //    uint32 pNewEmployeePoolPromille, uint32 ptotalPoolOptions, bytes pESOPLegalWrapperIPFSHash)
             var ipfsHash = new Buffer("QmRsjnNkEpnDdmYB7wMR7FSy1eGZ12pDuhST3iNLJTzAXF", 'ascii');
+            await deployer.deploy(CALCULATOR, 1 * years, 4 * years, 8000, 2000, 1000);
+            let optcals = await CALCULATOR.deployed();
+            await deployer.deploy(EMPLIST);
+            let emplist = await EMPLIST.deployed();
+            await emplist.transferOwnership(ESOP.address);
             deployer.logger.log('opening esop');
-            let tx = await esop.openESOP(1 * years, 4 * years, 8000, 2000, 1000, 1000000, web3.toBigNumber('0x' + ipfsHash.toString('hex')));
+            let tx = await esop.openESOP(CALCULATOR.address, EMPLIST.address, 1000000, web3.toBigNumber('0x' + ipfsHash.toString('hex')));
             if (tx.logs.some(e => e.event == 'ReturnCode')) {
                 // error code returned
                 deployer.logger.log(`openESOP returned rc: ${tx.logs[0].args['rc']}`);
