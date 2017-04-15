07757913a059e201c70ae9283f4221ea7c848869
Marcin Rudolf <marcin.rudolf@neufund.org>
2017-04-15 22:47:21 +0200

    force 4 mln block gas limit for dev network in truffle

--- truffle.js
+++ truffle.js
@@ -5,6 +5,7 @@ module.exports = {
     development: {
       host: "localhost",
       port: 8545,
+      gas: 4000000, // close to current mainnet limit
       network_id: "*" // Match any network id
     },
     paritydev: {
