4d24122426e4c5c030236df4fd9dacd4b1b1f7ca
Marcin Rudolf <marcin.rudolf@neufund.org>
2017-04-17 00:54:44 +0200

    ESOP renamed to oldESOP in ESOPMigrations

--- contracts/ESOPMigration.sol
+++ contracts/ESOPMigration.sol
@@ -2,18 +2,18 @@ pragma solidity ^0.4.0;
 import "./ESOPTypes.sol";
 
 contract ESOPMigration {
-  modifier onlyESOP() {
-    if (msg.sender != getESOP())
+  modifier onlyOldESOP() {
+    if (msg.sender != getOldESOP())
       throw;
     _;
   }
 
   // returns ESOP address which is a sole executor of exerciseOptions function
-  function getESOP() public constant returns (address);
+  function getOldESOP() public constant returns (address);
 
   // migrate employee to new ESOP contract, throws if not possible
   // in simplest case new ESOP contract should derive from this contract and implement abstract methods
   // employees list is available for inspection by employee address
   // poolOptions and extraOption is amount of options transferred out of old ESOP contract
-  function migrate(address employee, uint poolOptions, uint extraOptions) onlyESOP public;
+  function migrate(address employee, uint poolOptions, uint extraOptions) onlyOldESOP public;
 }
