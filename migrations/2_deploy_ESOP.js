030a6dc482fea01fe75fba1cb355aea3c6532780
Marcin Rudolf <marcin.rudolf@neufund.org>
2017-04-19 21:57:01 +0200

    deployment script updated to match new constructors

--- migrations/2_deploy_ESOP.js
+++ migrations/2_deploy_ESOP.js
@@ -2,20 +2,30 @@ require('babel-register');
 
 const RoT = artifacts.require("RoT");
 const ESOP = artifacts.require("ESOP");
+const CALCULATOR = artifacts.require("OptionsCalculator");
+const EMPLIST = artifacts.require("EmployeesList");
 
-module.exports = function (deployer, network) {
+module.exports = function (deployer, network, accounts) {
     deployer.then(async function () {
         await deployer.deploy(RoT);
         let rot = await RoT.deployed();
-        var ceoAddr;
+        var companyAddress;
         if (network === 'live')
-            ceoAddr = ''; // provide company address that will manage contract on live network
+            companyAddress = ''; // provide company address that will manage contract on live network
         else {
-            ceoAddr = await rot.owner(); // on other networks deploying account is the company
+            // 0 is default account, make company to use account 2
+            companyAddress = accounts[1]; // on other networks deploying account is the company
         }
-        deployer.logger.log(`Assuming account ${ceoAddr} as a company`);
-        await deployer.deploy(ESOP, ceoAddr, RoT.address);
+        deployer.logger.log(`Assuming account ${companyAddress} as a company`);
+        await deployer.deploy(CALCULATOR, companyAddress);
+        let optcalc = await CALCULATOR.deployed();
+        // company owns options calculator
+        await deployer.deploy(EMPLIST);
+        let emplist = await EMPLIST.deployed();
+        await deployer.deploy(ESOP, companyAddress, RoT.address, CALCULATOR.address, EMPLIST.address);
+        // esop contract owns employee's list
+        await emplist.transferOwnership(ESOP.address);
         deployer.logger.log(`Setting ESOP address in RoT to ${ESOP.address}`);
-        await rot.setESOP(ESOP.address, ceoAddr);
+        await rot.setESOP(ESOP.address, companyAddress);
     });
 };
--- migrations/4_set_esop_and_employees.js
+++ migrations/4_set_esop_and_employees.js
@@ -2,7 +2,6 @@ require('babel-register');
 
 const ESOP = artifacts.require("ESOP");
 const CALCULATOR = artifacts.require("OptionsCalculator");
-const EMPLIST = artifacts.require("EmployeesList");
 const years = 365 * 24 * 60 * 60;
 
 module.exports = function (deployer, network, accounts) {
@@ -10,25 +9,22 @@ module.exports = function (deployer, network, accounts) {
     if (network !== 'live') {
         deployer.then(async function () {
             let esop = await ESOP.deployed();
+            let companyAddress = await esop.companyAddress();
             // open esop
             // function openESOP(uint32 pcliffPeriod, uint32 pvestingPeriod, uint32 pMaxFadeoutPromille, uint32 pbonusOptionsPromille,
             //    uint32 pNewEmployeePoolPromille, uint32 ptotalPoolOptions, bytes pESOPLegalWrapperIPFSHash)
             var ipfsHash = new Buffer("QmRsjnNkEpnDdmYB7wMR7FSy1eGZ12pDuhST3iNLJTzAXF", 'ascii');
-            await deployer.deploy(CALCULATOR, 1 * years, 4 * years, 8000, 2000, 1000, 500);
-            let optcals = await CALCULATOR.deployed();
-            await deployer.deploy(EMPLIST);
-            let emplist = await EMPLIST.deployed();
-            await emplist.transferOwnership(ESOP.address);
+            deployer.logger.log('initializing options converter');
+            let optcalc = await CALCULATOR.deployed();
+            await optcalc.setParameters(1 * years, 4 * years, 8000, 2000, 1000, 500, {from: companyAddress});
             deployer.logger.log('opening esop');
-            let tx = await esop.openESOP(CALCULATOR.address, EMPLIST.address, 1000000, web3.toBigNumber('0x' + ipfsHash.toString('hex')));
+            let tx = await esop.openESOP(1000000, web3.toBigNumber('0x' + ipfsHash.toString('hex')), {from: companyAddress});
             if (tx.logs.some(e => e.event === 'ReturnCode')) {
                 // error code returned
                 deployer.logger.log(`openESOP returned rc: ${tx.logs[0].args['rc']}`);
                 throw `openESOP returned rc: ${tx.logs[0].args['rc']}`;
             }
             deployer.logger.log('esop opened');
-            //let company = await esop.companyAddress();
-
         } );
     }
 };
