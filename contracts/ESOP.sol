b83bef0232f91017b4c9bef77f1187ee25755a93
Marcin Rudolf <marcin.rudolf@neufund.org>
2017-06-16 17:27:27 +0200

    bumped solidity version to 0.4.11

--- contracts/ESOP.sol
+++ contracts/ESOP.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.8;
+pragma solidity ^0.4.11;
 import "./ESOPTypes.sol";
 import "./EmployeesList.sol";
 import "./OptionsCalculator.sol";
--- sol_tests/Test.ESOPMigration.sol
+++ sol_tests/Test.ESOPMigration.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.8;
+pragma solidity ^0.4.11;
 
 import 'dapple/test.sol';
 import 'dapple/reporter.sol';
